b28de556649c7fc147b9870c1aad571b5e222f03
Sydney Hunter <sydney.jane.hunter@gmail.com>
2019-11-12 15:17:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,9 @@ end
 def summon_captain_planet(array)
   planeteer_calls = []
   array.map do |calls| 
-    planeteer_calls << calls.capitalize! 
-  # Your code here
+    planeteer_calls << "#{calls.capitalize}!" 
+  end 
+  planeteer_calls
 end
 
 def long_planeteer_calls# code an argument here
