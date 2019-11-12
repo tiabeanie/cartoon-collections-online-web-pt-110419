d028bc9b101d32fb9a3fda56bc6287159891c9cf
Sydney Hunter <sydney.jane.hunter@gmail.com>
2019-11-12 15:14:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,8 @@ def roll_call_dwarves(array)
   array.each_with_index {|name, index| puts "#{index + 1}. #{name}."}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)
+  
   # Your code here
 end
 
